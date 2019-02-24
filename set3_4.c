7b97ab3a0386dddd305e2a94a171ed3123dbe51f
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-02-24 05:13:50 -0800

    Create set3_4.c

---
+++ set3_4.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+
+int main()
+{
+   int n,a[100],i,temp,j,k;
+   printf("enter the count");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(j=1;j<=n;j++)
+   {
+      for(k=j+1;k<=n;k++)
+      {
+        if(a[j]>=a[k])
+        {
+        temp=a[j];
+        a[j]=a[k];
+        a[k]=temp;
+            
+        }
+      }
+    printf(" %d\t",a[j]);
+   }
+    
+}
